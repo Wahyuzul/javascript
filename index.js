7fb37deb436c8a1367f9f186a92d65042097e3d5
Wahyu Zul Fahmi <Wahyuzulfahmi14@gmail.com>
2023-08-14 22:17:38 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// segment 1
+// soal 1
+// let age = prompt ("Enter your age:");
+// if (age >= 18) {
+//     console.log("You are old enough to drive");
+// } else {
+//     console.log("You are left with " + (18 - age) + " years to drive");
+// }
+
+//soal 2
+// let myAge = prompt ("Enter your age:");
+// let yourAge = prompt ("Enter your age:");
+// if (myAge > yourAge) {
+//     console.log("I am " + (myAge - yourAge) + " years older than you");
+// } else if (myAge < yourAge) {
+//     console.log("You are " + (yourAge - myAge) + " years older than me");
+// } else {
+//     console.log("We have the same age");
+// }
+
+//soal 3
+let a = 2;
+let b = 3;
+let result = (a > b) ? "a is greater than b" : "a is less than or equal to b";
+console.log(result);
